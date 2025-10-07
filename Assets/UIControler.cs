a4cdefca479fe8735a8247f1c52de527b02b9e3a
nognight1203 <b22583338@whi.com.tw>
2025-10-07 16:17:21 +0800

    [add]add button

--- Assets/UIControler.cs
+++ Assets/UIControler.cs
@@ -8,6 +8,11 @@ public class UIControler : MonoBehaviour
 
     public GameObject UI;
     public GameObject PaintsScrollView;
+
+    [Header("Add Mode")]
+    public GameObject Add;
+    public GameObject Done;
+    public GameObject Cancel;
     
 
     public void ShowChosingPaintsUI()
@@ -21,6 +26,31 @@ public class UIControler : MonoBehaviour
         PaintsScrollView.SetActive(false);
         Setting.SetActive(true);
     }
+
+    //AddMode
+    public void AddModeON()
+    {
+        Add.SetActive(false);
+        Done.SetActive(true);
+        Cancel.SetActive(true);
+        PointTrigger.AddMode = true;
+    }
+    public void AddModeOFF()
+    {
+        Add.SetActive(true);
+        Done.SetActive(false);
+        Cancel.SetActive(false);
+        PointTrigger.AddMode = false;
+    }
+    public void AddDone()
+    {
+        PointTrigger.newPaint = null;
+    }
+    public void AddCancel()
+    {
+
+    }
+
 
 
 }
